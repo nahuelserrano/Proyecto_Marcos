8bfd6db04e18062fbb2746f4a828738ef7202173
GianFrancoLaplace <laplacegian@gmail.com>
2025-02-27 15:52:14 -0300

    Primeras implementaciones del Viajeservices y creación de CamionService y ChoferServices

---
+++ Proyecto_Marcos.Presentacion/Services/CamionService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static ChoferService;
+using System.Threading.Tasks;
+using TransporteApp.Utils;
+using TransporteApp.Models;
+
+
+public class CamionService
+{
+    private CamionRepository _camionRepository;
+    
+    public CamionService(CamionRepository camionR)
+    {
+        this._camionRepository = camionR ?? throw new ArgumentNullException(nameof(camionR));
+    }
+
+    public async Task<Result<int>> getByIdAsync(int id)
+    {
+        if (id <= 0)
+            return Result<int>.Failure("El id no puede ser menor a 0");
+
+        Camion camion = await this._camionRepository.getById(id);
+
+        if (camion == null)
+            return Result<int>.Failure("El camion con el id " + id + " No existe");
+
+        return Result<int>.Success(id);
+    }
+
+    internal async Task<Camion> ObtenerCamionAsync(object camionId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Proyecto_Marcos.Presentacion/Services/ChoferService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using TransporteApp.Models;
+using TransporteApp.Utils;
+
+public class ChoferService
+{
+    private ChoferRepository _choferRepository;
+	
+	public ChoferService(ChoferRepository choferRepository)
+    {
+        this._choferRepository = choferRepository ?? throw new ArgumentNullException(nameof(choferRepository));
+    }
+
+    public async Task<Result<int>> getByIdAsync(int id)
+    {
+        if (id <= 0)
+            return Result<int>.Failure("El id no puede ser menor a 0");
+
+        Chofer chofer = await this._choferRepository.getById(id);
+
+        if (chofer == null)
+            return Result<int>.Failure("El chofer con el id " + id + " No existe");
+        
+        return Result<int>.Success(id);
+    }
+
+}
