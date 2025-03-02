d787970fdd80cd11198e4238aed305c0daea34c8
GianFrancoLaplace <laplacegian@gmail.com>
2025-03-02 12:34:00 -0300

    Estandirazacion de nombres de archivos y directorios, arreglo de problemas en el archivo .csproj

--- Proyecto_Marcos.Presentacion/models/cheque.cs
+++ Proyecto_Marcos.Presentacion/models/cheque.cs
@@ -1,29 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Proyecto_Marcos.Presentacion.models
+public class Class1
 {
-   public class Cheque
-    {
-
-        public Cliente Cliente_Dueño_Cheque { get; set; }
-        public DateTime FechaIngresoCheque { get; set; }
-        public int NumeroCheque { get; set; }
-        public float Monto { get; set; }
-        public String Banco { get; set; }
-        public DateTime FechaCobro { get; set; }
-
-        public Cheque(Cliente cliente, DateTime FechaIngresoCheque, int NumeroCheque, float Monto, String Banco, DateTime FechaCobro)
-        {
-            this.Cliente_Dueño_Cheque = cliente;
-            this.FechaIngresoCheque = DateTime.Now;
-            this.NumeroCheque = NumeroCheque ;
-            this.Monto = Monto;
-            this.Banco = Banco;
-            this.FechaCobro = FechaCobro;
-        }
-    }
+	public Class1()
+	{
+	}
 }
