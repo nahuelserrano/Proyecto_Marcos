775599e9c831f60c33817e59137745c6d528b2ff
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-03-07 14:42:36 -0300

    creo la db y comienzo a establecer la conexión

---
+++ Proyecto_Marcos.Presentacion/DataBase_Helper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+
+namespace Proyecto_Marcos.Presentacion
+{
+
+    public class DataBase_Helper
+    {
+        private string connectionString = "Data Source=truck_manager_db.db"; 
+
+        public void Connect()
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Conexión exitosa!");
+            }
+        }
+    }
+}
