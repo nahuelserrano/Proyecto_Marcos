ce4ed65f03f92fbbb7ee19d54c5cf262313c5c87
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-03-08 13:46:12 -0300

    conexion en proceso

---
+++ Proyecto_Marcos.Presentacion/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SQLite;
+
+class Conexion
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=truck_manager_db.db;Version=3;";
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+
+            // Crear la tabla 'users' si no existe
+            string createTableSql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)";
+            using (SQLiteCommand createCommand = new SQLiteCommand(createTableSql, connection))
+            {
+                createCommand.ExecuteNonQuery();
+            }
+
+            Console.WriteLine("Base de datos y tabla 'users' creadas exitosamente.");
+
+            // Listar todas las tablas en la base de datos
+            string listTablesSql = "SELECT name FROM truck_manager_db.db WHERE type='table';";
+            using (SQLiteCommand listCommand = new SQLiteCommand(listTablesSql, connection))
+            {
+                using (SQLiteDataReader reader = listCommand.ExecuteReader())
+                {
+                    Console.WriteLine("Tablitassss en la base de datos:");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("- " + reader["name"]);
+                    }
+                }
+            }
+        }
+    }
+}
+
--- Proyecto_Marcos.Presentacion/Datos_pagos.cs
+++ Proyecto_Marcos.Presentacion/Datos_pagos.cs
@@ -4,42 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
-using MySql.Data;
-using MySql.Data.MySqlClient;
 
 namespace Proyecto_Marcos.Presentacion
 {
     public class Datos_pagos
     {
-        public DataTable Listado_pagos(String cTexto)
-        {
-            MySqlDataReader Resultado;
-            DataTable tabla = new DataTable();
-            MySqlConnection SqlCon = new MySqlConnection();
+        //public DataTable Listado_pagos(String cTexto)
+        //{
+        //    MySqlDataReader Resultado;
+        //    DataTable tabla = new DataTable();
+        //    MySqlConnection SqlCon = new MySqlConnection();
 
-            try
-            {
-                SqlCon = Conexion.getInstancia().CrearConexion();
-                String sql_tarea = "SELECT * FROM pago where Pagado like '"+cTexto+"' ";
-                MySqlCommand comando = new MySqlCommand(sql_tarea,SqlCon);
-                comando.CommandTimeout = 60;
-                SqlCon.Open();
-                Resultado = comando.ExecuteReader();
-                tabla.Load(Resultado);
-                Console.WriteLine(cTexto);
-                return tabla;
+        //    try
+        //    {
+        //        SqlCon = Conexion.getInstancia().CrearConexion();
+        //        String sql_tarea = "SELECT * FROM pago where Pagado like '"+cTexto+"' ";
+        //        MySqlCommand comando = new MySqlCommand(sql_tarea,SqlCon);
+        //        comando.CommandTimeout = 60;
+        //        SqlCon.Open();
+        //        Resultado = comando.ExecuteReader();
+        //        tabla.Load(Resultado);
+        //        Console.WriteLine(cTexto);
+        //        return tabla;
 
 
-            }
-            catch(Exception ex)
-            {
-                throw ex;
-            }
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        throw ex;
+        //    }
 
-            finally
-            {
-                if (SqlCon.State==ConnectionState.Open)  SqlCon.Close(); 
-            }
-        }
+        //    finally
+        //    {
+        //        if (SqlCon.State==ConnectionState.Open)  SqlCon.Close(); 
+        //    }
+        //}
     }
 }
